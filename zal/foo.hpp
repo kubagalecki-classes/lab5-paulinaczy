9ea2a24dce9bc5fde74b5e3ed0e20a5c58d3499f
paulinaczy <73669289+paulinaczy@users.noreply.github.com>
2020-12-09 20:45:26 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -5,23 +5,24 @@
 #include <list>
 #include <vector>
 #include <iterator>
+using std::iterator;
 std::vector< char > foo(std::list< Human >& people)
 {
     	std::vector<char> wektor(people.size());
-	std::list<Human> iterator iteratorek = people.begin();
-	std::vector<char> reverse_iterator reverse_iteratorek = wektor.rbegin();
-	for (iteratorek; iteratorek != people.end(); iteratorek++)
+	std::list<Human>::iterator i = people.begin();
+	std::vector<char>::reverse_iterator r = wektor.rbegin();
+	for (i; i != people.end(); i++)
 	{
-		iteratorek->birthday();
-		if (iteratorek->isMonster()==true)
+		i->birthday();
+		if (i->isMonster()==true)
 		{
-			*reverse_iteratorek = 'n';
+			*r = 'n';
 		}
 		else
 		{
-			*reverse_iteratorek = 'y';
+			*r = 'y';
 		}
-		reverse_iteratorek++;
+		r++;
 	}
 	return wektor;
 }
