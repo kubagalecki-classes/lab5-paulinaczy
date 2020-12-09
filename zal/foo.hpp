4aa80fba9618920edf8705de2af53ab5a55eef39
paulinaczy <73669289+paulinaczy@users.noreply.github.com>
2020-12-09 15:41:46 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -8,8 +8,8 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector<char> wektor(people.size());
-	std::list<Human>::std::iterator iteratorek = people.begin();
-	std::vector<char>::std::reverse_iterator reverse_iteratorek = wektor.rbegin();
+	std::list<Human>std::iterator iteratorek = people.begin();
+	std::vector<char>std::reverse_iterator reverse_iteratorek = wektor.rbegin();
 	for (iteratorek; iteratorek != people.end(); iteratorek++)
 	{
 		iteratorek->birthday();
